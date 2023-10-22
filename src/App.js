000e983316bf01f2ea1e39070c971c25d64ef02e
dhruvgoyal9999 <142114666+dhruvgoyal9999@users.noreply.github.com>
2023-10-23 00:59:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Calculator } from './components/Calculator';
+
+function App() {
+  return (
+    <div>
+      <h2>React Task-5 DHRUV</h2>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
